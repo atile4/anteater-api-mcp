9b09bfe39bb3aebb6d88d61e350474c567bb63e9
atile4 <atticus1467@gmail.com>
2026-09-05 15:45:43 -0700

    feat: add all other columns of a Course and set unused to null

--- src/anteater_api_mcp/client/models.py
+++ src/anteater_api_mcp/client/models.py
@@ -1,9 +1,39 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Course(BaseModel):
+    id: str | None = None
     department: str
     courseNumber: str
-    title: str
-    description: str
+    courseNumeric: int | None = None
     school: str
+    title: str
     courseLevel: str
+    minUnits: float | None = None
+    maxUnits: float | None = None
+    description: str
+    departmentName: str | None = None
+    
+    # TODO: Implement Instructor model for elements in this list
+    instructors: list[dict] | None = None
+    
+    # TODO: Implement PrerequisiteTree model for structure matching AND/OR logic
+    prerequisiteTree: dict | None = None
+    
+    prerequisiteText: str | None = None
+    
+    # TODO: Implement PrerequisiteRef model for elements in this list
+    prerequisites: list[dict] | None = None
+    
+    dependencies: list[str] | None = None
+    repeatability: str | None = None
+    repeatabilityTimes: int | None = None
+    repeatabilityType: str | None = None
+    gradingOption: str | None = None
+    concurrent: str | None = None
+    sameAs: str | None = None
+    restriction: str | None = None
+    overlap: str | None = None
+    corequisites: str | None = None
+    geList: list[str] | None = None
+    geText: str | None = None
+    terms: list[str] | None = None
