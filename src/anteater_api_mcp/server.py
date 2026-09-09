cd10074ff18d41ea350b4f15177f30643dee4b7b
atile4 <atticus1467@gmail.com>
2026-09-09 11:43:57 -0700

    refactor: rename to courses

--- src/anteater_api_mcp/server.py
+++ src/anteater_api_mcp/server.py
@@ -1,5 +1,5 @@
 from anteater_api_mcp.app import mcp
-import anteater_api_mcp.tools.retrieve_courses
+import anteater_api_mcp.tools.courses
 import anteater_api_mcp.tools.retrieve_programs
 import anteater_api_mcp.tools.ap_exams
 import anteater_api_mcp.tools.degree_process
