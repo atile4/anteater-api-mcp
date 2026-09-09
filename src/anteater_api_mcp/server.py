6ee39ad1b31006c437b8530ead7a586a91e24707
atile4 <atticus1467@gmail.com>
2026-09-08 22:41:24 -0700

    rename because retrieve word in files is not necessary

--- src/anteater_api_mcp/server.py
+++ src/anteater_api_mcp/server.py
@@ -1,7 +1,7 @@
 from anteater_api_mcp.app import mcp
 import anteater_api_mcp.tools.retrieve_courses
 import anteater_api_mcp.tools.retrieve_programs
-import anteater_api_mcp.tools.retrieve_ap_exams
+import anteater_api_mcp.tools.ap_exams
 import anteater_api_mcp.tools.degree_process
 
 def run():
