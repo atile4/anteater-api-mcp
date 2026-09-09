10b748366dec98aba97c3b890f54e22dcd376d4c
atile4 <atticus1467@gmail.com>
2026-09-09 15:46:56 -0700

    fix: rename to degree_progress to fix typo

--- src/anteater_api_mcp/server.py
+++ src/anteater_api_mcp/server.py
@@ -2,7 +2,7 @@ from anteater_api_mcp.app import mcp
 import anteater_api_mcp.tools.courses
 import anteater_api_mcp.tools.retrieve_programs
 import anteater_api_mcp.tools.ap_exams
-import anteater_api_mcp.tools.degree_process
+import anteater_api_mcp.tools.degree_progress
 import anteater_api_mcp.tools.electives
 
 def run():
